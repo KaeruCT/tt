7236062081046a90364262e2acfe7c74a151d6e6
Andrés Villarreal <avi@hellofresh.com>
2019-10-10 19:49:48 +0200

    Build output as of 1a20fd4274adb0b3aa7a2fa27ec8c11d30f5af3c

--- src/index.js
+++
@@ -1,8 +0,0 @@
-
-require('normalize.css/normalize.css');
-require('./styles/index.scss');
-require('./game/index.js');
-
-window.addEventListener('touchmove', function(event) {
-    event.preventDefault();
-}, false);
